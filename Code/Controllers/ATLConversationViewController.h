2602f573cefd7abfeb5b3372a2a4244dc30d33dc
Sergio Gutierrez <sergio@gokarumi.com>
2017-04-05 16:52:35 +0200

    Expose shouldDisplaySenderLabelForSection method

--- Code/Controllers/ATLConversationViewController.h
+++ Code/Controllers/ATLConversationViewController.h
@@ -161,13 +161,13 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  @abstract Asks the data source to configure the default query used to fetch content for the controller if necessary.
  @discussion The `LYRConversationViewController` uses the following default query:
- 
+
      LYRQuery *query = [LYRQuery queryWithQueryableClass:[LYRMessage class]];
      query.predicate = [LYRPredicate predicateWithProperty:@"conversation" predicateOperator:LYRPredicateOperatorIsEqualTo value:self.conversation];
      query.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"position" ascending:YES]];
- 
+
  Applications that require advanced query configuration can do so by implementing this data source method.
- 
+
  @param viewController The `ATLConversationViewController` requesting the configuration.
  @param defaultQuery An `LYRQuery` object with the default configuration for the controller.
  @return An `LYRQuery` object with any additional configuration.
@@ -261,6 +261,8 @@ NS_ASSUME_NONNULL_BEGIN
  @param message The Message object to send.
  */
 - (void)sendMessage:(LYRMessage *)message;
+
+- (BOOL)shouldDisplaySenderLabelForSection:(NSUInteger)section;
 
 ///---------------------------
 /// @name Configuring Behavior
